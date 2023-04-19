db5b74b8b5e33637b68ce58e17396bd1f89369a6
Jerry Peng <jerrypeng@macbook-pro.myfiosgateway.com>
2023-04-19 01:17:32 -0400

    fixed bugs

--- playground.py
+++ playground.py
@@ -3,13 +3,73 @@ import numpy as np
 from PIL import Image
 import torch 
 from torch.utils.data import Dataset
+from torchvision import transforms
 
 
 class FrameDataset(Dataset):
-    def __init__(self, root_dir, mode, transform=None):
+    def __init__(self, root_dir, labeled=True, transform=None):
+        """
+        Args:
+            root_dir (string): Dataset folder path
+            labeled (bool, optional): If the input data is labeled. Defaults to True.
+            transform (torchvision.transforms, optional): Data transforms. Defaults to None.
+        """
         self.root_dir = root_dir
-        self.mode = mode 
-        self.transform = transform 
+        self.labeled = labeled
+        self.transform = transform
+        self.video_dirs = sorted([d for d in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, d))])
         
-        self.mode_path = os.path.join(root_dir, mode)
-        self.vedios = [os.path.join(root_dir, subfolder) for subfolder in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, subfolder))]
+    def __len__(self):
+        return len(self.video_dirs)
+    
+    def __getitem__(self, idx):
+        video_dir = os.path.join(self.root_dir, self.video_dirs[idx])
+        frame_files = sorted([f for f in os.listdir(video_dir) if f.endswith('.png')])
+        
+        frames = []
+        for frame_file in frame_files:
+            frame_path = os.path.join(video_dir, frame_file)
+            frame = Image.open(frame_path).convert('RGB')
+            if self.transform:
+                frame = self.transform(frame)
+            frames.append(frame)
+            
+        frames = torch.stack(frames)
+        
+        if self.labeled:
+            mask_path = os.path.join(video_dir, 'mask.npy')
+            mask = np.load(mask_path)
+            mask = torch.from_numpy(mask).long()
+            return frames, mask
+        else:
+            return frames, None
+
+
+def dataset_test():
+    train = "/scratch/jp4906/VideoMask/train"
+    unlabeled = "/scratch/jp4906/VideoMask/unlabeled"
+    val = "/scratch/jp4906/VideoMask/val"
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    train_dataset = FrameDataset(root_dir=train, labeled=True, transform=transform)
+    print("Train dataset has {} length".format(len(train_dataset)))
+    print("")
+    video_frames, video_mask = train_dataset[0]
+    print('video_frames for train size: {}'.format(video_frames.size()))
+    print("")
+    print('video_mask for train size: {}'.format(video_mask.shape))
+    print("")
+    print("====================")
+
+    unlabeled_dataset = FrameDataset(root_dir=unlabeled, labeled=False, transform=transform)
+    print("Train dataset has {} length".format(len(train_dataset)))
+    print("")
+    video_frames, video_mask = unlabeled_dataset[0]
+    print('video_frames for unlabeled size: {}'.format(video_frames.size()))
+    print("")
+    print('video_mask for unlabeled should be NoneType: {}'.format(video_mask))
+    print("")
+    print("====================")
+
--- src/dataset.py
+++ src/dataset.py
@@ -1,93 +1,74 @@
-import torch
 import os
 import numpy as np
-import torch
-import torch.utils.data as data
 from PIL import Image
+import torch 
+from torch.utils.data import Dataset
 from torchvision import transforms
-import matplotlib.pyplot as plt
 
-class VideoFramesDataset(data.Dataset):
-    def __init__(self, root, mode, transform=None):
-        self.root = root
-        self.mode = mode # train, val, unlabeled
+
+class FrameDataset(Dataset):
+    def __init__(self, root_dir, labeled=True, transform=None):
+        """
+        Args:
+            root_dir (string): Dataset folder path
+            labeled (bool, optional): If the input data is labeled. Defaults to True.
+            transform (torchvision.transforms, optional): Data transforms. Defaults to None.
+        """
+        self.root_dir = root_dir
+        self.labeled = labeled
         self.transform = transform
-
-        self.video_names = os.listdir(os.path.join(self.root, self.mode))
-        # remove .DS_Store
-        if '.DS_Store' in self.video_names:
-            self.video_names.remove('.DS_Store')
-
-
-        self.video_paths = [os.path.join(self.root, self.mode, video_name) for video_name in self.video_names]
-
-
-        self.video_frame_paths = []
-        for video_path in self.video_paths:
-            # frame_paths: list of frame paths, image_0.png, image_1.png, ...
-            # exclude mask.npy
-            frame_paths = [os.path.join(video_path, frame_name) for frame_name in os.listdir(video_path) if frame_name != 'mask.npy']
-            self.video_frame_paths.append(frame_paths)
-
-        # sort frame paths by number in file name
-        for frame_paths in self.video_frame_paths:
-            frame_paths.sort(key=lambda x: int(x.split('/')[-1].split('.')[0].split('_')[-1]))
-
-            
-        self.video_frame_masks = []
-        if self.mode != 'unlabeled':
-            # load mask, mask.npy contains mask for each frame
-            for video_path in self.video_paths:
-                mask_path = os.path.join(video_path, 'mask.npy')
-                mask = np.load(mask_path)
-                self.video_frame_masks.append(mask)
-
-
-    def __getitem__(self, index):
-        video_frames = []
-        for frame_path in self.video_frame_paths[index]:
-            frame = Image.open(frame_path)
-            if self.transform is not None:
-                frame = self.transform(frame)
-            video_frames.append(frame)
-        video_frames = torch.stack(video_frames, dim=0)
-
-        video_mask = self.video_frame_masks[index]
-
-        return video_frames, video_mask
-
+        self.video_dirs = sorted([d for d in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, d))])
+        
     def __len__(self):
-        return len(self.video_names)
+        return len(self.video_dirs)
     
-def show_img_and_mask(img, mask):
-    fig, ax = plt.subplots(1, 2)
-    ax[0].imshow(img)
-    ax[1].imshow(mask)
-    plt.show()
-    
-if __name__ == '__main__':
-    # test dataset
+    def __getitem__(self, idx):
+        video_dir = os.path.join(self.root_dir, self.video_dirs[idx])
+        frame_files = sorted([f for f in os.listdir(video_dir) if f.endswith('.png')])
+        
+        frames = []
+        for frame_file in frame_files:
+            frame_path = os.path.join(video_dir, frame_file)
+            frame = Image.open(frame_path).convert('RGB')
+            if self.transform:
+                frame = self.transform(frame)
+            frames.append(frame)
+            
+        frames = torch.stack(frames)
+        
+        if self.labeled:
+            mask_path = os.path.join(video_dir, 'mask.npy')
+            mask = np.load(mask_path)
+            mask = torch.from_numpy(mask).long()
+            return frames, mask
+        else:
+            return frames, None
+
+
+def dataset_test():
+    train = "/scratch/jp4906/VideoMask/train"
+    unlabeled = "/scratch/jp4906/VideoMask/unlabeled"
+    val = "/scratch/jp4906/VideoMask/val"
     transform = transforms.Compose([
         transforms.ToTensor(),
     ])
 
-    dataset = VideoFramesDataset(root='./data/', mode='train', transform=transform)
-    print('dataset size: {}'.format(len(dataset)))
+    train_dataset = FrameDataset(root_dir=train, labeled=True, transform=transform)
+    print("Train dataset has {} length".format(len(train_dataset)))
+    print("")
+    video_frames, video_mask = train_dataset[0]
+    print('video_frames for train size: {}'.format(video_frames.size()))
+    print("")
+    print('video_mask for train size: {}'.format(video_mask.shape))
+    print("")
+    print("====================")
 
-    video_frames, video_mask = dataset[0]
-    print('video_frames size: {}'.format(video_frames.size()))
-    print('video_mask size: {}'.format(video_mask.shape))
-
-    print (video_frames[0].shape)
-    print(video_mask[0].shape)
-
-    for i in range(0, 21):
-        img = video_frames[i].numpy().transpose(1, 2, 0)
-        mask = video_mask[i]
-        show_img_and_mask(img, mask)
-
-    
-
-
-
-
+    unlabeled_dataset = FrameDataset(root_dir=unlabeled, labeled=False, transform=transform)
+    print("Train dataset has {} length".format(len(train_dataset)))
+    print("")
+    video_frames, video_mask = unlabeled_dataset[0]
+    print('video_frames for unlabeled size: {}'.format(video_frames.size()))
+    print("")
+    print('video_mask for unlabeled should be NoneType: {}'.format(video_mask))
+    print("")
+    print("====================")
